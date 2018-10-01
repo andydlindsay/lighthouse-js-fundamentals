814540ca8b78696d4feb185d9482bab29869dbda
Andy Lindsay <andydlindsay@gmail.com>
2018-10-01 21:44:24 +0000

    hello.js exercise with 'sayHello' function completed

---
+++ hello.js
@@ -0,0 +1,13 @@
+// function sayHello() {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+function sayHello(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
